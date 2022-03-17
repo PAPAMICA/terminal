af4adee5eaecf71a72a2b98e35ec9298e1ff0468
PAPAMICA <mickael@papamica.com>
2022-03-17 16:08:27 +0100

    add cheat alias

--- server_utils.sh
+++ server_utils.sh
@@ -125,7 +125,8 @@ gzip -d cheat-linux-amd64.gz > /dev/null 2> /dev/null && \
 chmod +x cheat-linux-amd64 && mv cheat-linux-amd64 /usr/local/bin/cheat && rm -rf cheat-linux-amd64* && \
 git clone https://github.com/PAPAMICA/cheatsheets.git ~/.config/cheat/cheatsheets/papamica/ && \
 curl -s "https://raw.githubusercontent.com/PAPAMICA/terminal/main/cheat_config" ~/.config/cheat/conf.yml'
-zshrc='alias "?"="cheat"'
+zshrc='alias "?"="cheat"
+alias "??"="cheat perso"'
 app_install $app $install $zshrc
 
 ## duf
