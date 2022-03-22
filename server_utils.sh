51d1b4dcfdae0cbe913c624e823d2f305cec7852
PAPAMICA <mickael@papamica.com>
2022-03-22 14:41:20 +0100

    fix

--- server_utils.sh
+++ server_utils.sh
@@ -25,7 +25,7 @@ fi
 # Update System
 echo ""
 echo "-- Update  --"
-apt-get update $VERBOSE
+eval apt-get update $VERBOSE
 UPGRADE=$(apt update 2>/dev/null | tail -1)
 echo " ℹ️  $UPGRADE"
 
@@ -35,7 +35,7 @@ echo " ℹ️  $UPGRADE"
 # Installation
 ## Requierements
 apt_install () {
-    apt-get install -y $1 $VERBOSE
+    eval apt-get install -y $1 $VERBOSE
     if [ $? -eq 0 ]; then
         echo "   ($i/8) ✅ $1"
     else
@@ -81,7 +81,7 @@ app_install () {
 
 ## git
 app='git'
-install='apt-get install -y git "'"$VERBOSE"'"'
+install='apt-get install -y git '$VERBOSE''
 zshrc="gic() { eval 'git add . && git commit -a -m \"'\$@'\" && git push'}
 alias gaa=\"git add *\"
 alias ga=\"git add\"
@@ -91,28 +91,28 @@ app_install $app $install $zshrc
 
 ## zsh
 app='zsh'
-install='apt-get install -y zsh "'"$VERBOSE"'" && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended "'"$VERBOSE"'" && \
-sed -i -e "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME="agnoster"/g" ~/.zshrc "'"$VERBOSE"'"'
+install='apt-get install -y zsh '$VERBOSE' && \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended '$VERBOSE' && \
+sed -i -e "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME="agnoster"/g" ~/.zshrc '$VERBOSE''
 zshrc=''
 app_install $app $install $zshrc
 
 ## ad
 app='ad'
-install='pip3 install advance-touch "'"$VERBOSE"'"'
+install='pip3 install advance-touch '$VERBOSE''
 zshrc='alias add="python3 ~/Library/Python/3.9/lib/python/site-packages/advance_touch.py"'
 app_install $app $install $zshrc
 
 ## atuin
 app='atuin'
-install='bash -c "$(curl -s https://raw.githubusercontent.com/ellie/atuin/main/install.sh)" "'"$VERBOSE"'" && \
-atuin import auto "'"$VERBOSE"'"'
+install='bash -c "$(curl -s https://raw.githubusercontent.com/ellie/atuin/main/install.sh)" '$VERBOSE' && \
+atuin import auto '$VERBOSE''
 zshrc='eval "$(atuin init zsh)"'
 app_install $app $install $zshrc
 
 ## bat
 app='batcat'
-install='apt-get install -y bat "'"$VERBOSE"'"'
+install='apt-get install -y bat '$VERBOSE''
 zshrc="alias cat='batcat --style=\"header\" --paging=never'
 alias bat='batcat --style=\"header\" --paging=never'
 alias catn='batcat --pager \"less -RF\"'
@@ -127,8 +127,8 @@ curl -s https://api.github.com/repos/aristocratos/btop/releases/latest \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - && \
-tar -xf btop-x86_64-linux-musl.tbz "'"$VERBOSE"'" && \
-make install "'"$VERBOSE"'" && cd .. && rm -rf btop'
+tar -xf btop-x86_64-linux-musl.tbz '$VERBOSE' && \
+make install '$VERBOSE' && cd .. && rm -rf btop'
 zshrc='alias top="btop --utf-force"
 alias btop="btop --utf-force"
 alias htop="btop --utf-force"'
@@ -141,20 +141,20 @@ install='curl -s https://api.github.com/repos/cheat/cheat/releases/latest \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - && \chdea
-gzip -d cheat-linux-amd64.gz "'"$VERBOSE"'" && \
+gzip -d cheat-linux-amd64.gz '$VERBOSE' && \
 chmod +x cheat-linux-amd64 && mv cheat-linux-amd64 /usr/local/bin/cheat && rm -rf cheat-linux-amd64* && \
-git clone https://github.com/PAPAMICA/cheatsheets.git ~/.config/cheat/cheatsheets/papamica/ "'"$VERBOSE"'" && \
+git clone https://github.com/PAPAMICA/cheatsheets.git ~/.config/cheat/cheatsheets/papamica/ '$VERBOSE' && \
 curl -s "https://raw.githubusercontent.com/PAPAMICA/terminal/main/cheat_conf.yml" >> ~/.config/cheat/conf.yml && \
 curl -s "https://raw.githubusercontent.com/PAPAMICA/terminal/main/cheat_autocomplete.zsh" >> ~/.oh-my-zsh/custom/cheat.zsh && \ 
 mkdir ~/.config/cheat/cheatsheets/personal/'
 zshrc="alias \"?\"=\"cheat\"
 alias \"??\"=\"cheat perso\"
-alias cheat-update='git -C ~/.config/cheat/cheatsheets/papamica/ pull "'"$VERBOSE"'" && echo \" ✅ Cheats updated !\"'"
+alias cheat-update='git -C ~/.config/cheat/cheatsheets/papamica/ pull '$VERBOSE' && echo \" ✅ Cheats updated !\"'"
 app_install $app $install $zshrc
 
 ## direnv
 app='direnv'
-install='apt install -y direnv "'"$VERBOSE"'"'
+install='apt install -y direnv '$VERBOSE''
 zshrc='eval "$(direnv hook zsh)"'
 app_install $app $install $zshrc
 
@@ -165,13 +165,13 @@ install='curl -s https://api.github.com/repos/muesli/duf/releases/latest \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - && \
-dpkg -i duf*.deb "'"$VERBOSE"'" && rm duf*.deb "'"$VERBOSE"'"'
+dpkg -i duf*.deb '$VERBOSE' && rm duf*.deb '$VERBOSE''
 zshrc=''
 app_install $app $install $zshrc
 
 ## exa
 app='exa'
-install='apt install -y exa "'"$VERBOSE"'"'
+install='apt install -y exa '$VERBOSE''
 zshrc='alias ls="exa -a --icons"         # short, multi-line
 alias ll="exa -1a --icons"        # list, 1 per line
 alias ld="ll"             # ^^^, NOTE: Trying to move to this for alternate hand commands
@@ -183,20 +183,20 @@ app_install $app $install $zshrc
 
 ## fd
 app='fdfind'
-install='apt install -y fd-find "'"$VERBOSE"'"'
+install='apt install -y fd-find '$VERBOSE''
 zshrc='alias locate="fdfind"
 alias find="fdfind"'
 app_install $app $install $zshrc
 
 ## fuck
 app='fuck'
-install='pip3 install thefuck  "'"$VERBOSE"'"'
+install='pip3 install thefuck  '$VERBOSE''
 zshrc='eval $(thefuck --alias)'
 app_install $app $install $zshrc
 
 ## micro
 app='micro'
-install='su - root -c "cd /usr/bin; wget -O- https://getmic.ro | GETMICRO_REGISTER=y sh; cd ~" "'"$VERBOSE"'"'
+install='su - root -c "cd /usr/bin; wget -O- https://getmic.ro | GETMICRO_REGISTER=y sh; cd ~" '$VERBOSE''
 zshrc=''
 app_install $app $install $zshrc
 
@@ -207,26 +207,26 @@ install='curl -s https://api.github.com/repos/BurntSushi/ripgrep/releases/latest
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi - && \
-dpkg -i ripgrep*.deb "'"$VERBOSE"'" && rm ripgrep*.deb "'"$VERBOSE"'"'
+dpkg -i ripgrep*.deb '$VERBOSE' && rm ripgrep*.deb '$VERBOSE''
 zshrc='alias grep="rg"'
 app_install $app $install $zshrc
 
 ## z
 app='z'
-install='git clone https://github.com/rupa/z.git /bin/z "'"$VERBOSE"'"'
+install='git clone https://github.com/rupa/z.git /bin/z '$VERBOSE''
 zshrc='. /bin/z/z.sh'
 app_install $app $install $zshrc
 
 ## zsh_autocompletion
 app='zsh_autocompletion'
-install='git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions "'"$VERBOSE"'"'
+install='git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions '$VERBOSE''
 zshrc='source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#808080"'
 app_install $app $install $zshrc
 
 ## zsh_syntax_highlighting
 app='zsh_syntax_highlighting'
-install='git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting "'"$VERBOSE"'"'
+install='git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting '$VERBOSE''
 zshrc='source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
 app_install $app $install $zshrc
 
